315fb7dbc819d3b1cc393b7cf8d5e0af41f9da44
Mirroslava <31761632+Mirroslava@users.noreply.github.com>
2017-10-06 09:15:51 +0300

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -7,9 +7,6 @@
 using namespace std;
 
 wstring cd(string command, string &path);
-
 void ls(WIN32_FIND_DATA winFileData, HANDLE  hFile);
-
 void playlist_create(string command, WIN32_FIND_DATA &winFileData, HANDLE  &hFile);
-
 void exit();
