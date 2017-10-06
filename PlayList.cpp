e8db7e45165758b5377c96d41f81fd60a4cf8d83
Mirroslava <31761632+Mirroslava@users.noreply.github.com>
2017-10-06 09:10:28 +0300

    Update PlayList.cpp

--- PlayList.cpp
+++ PlayList.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include "PlayList.h"
+
 using namespace std;
 
 PlayList::PlayList(string _name)
 {
 	playlist.open(_name.append(".m3u"));
 }
+
 void PlayList::out(string info)
 {
 	playlist<<info.c_str();
